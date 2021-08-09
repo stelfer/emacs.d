b58015a82a51a18e289a88e8c10723d948bba904
Soren Telfer <soren.telfer@gmail.com>
2021-08-09 12:09:07 -0700

    Cleaned up lsp-mode for local working before remote

--- lisp/my-lsp-mode.el
+++ lisp/my-lsp-mode.el
@@ -1,8 +1,9 @@
 
 (use-package lsp-mode
   :ensure t
-  :hook ((c++-mode . lsp-deferred)
-	 (c-mode . lsp-deferred))
+  :hook ((c++-mode . lsp)
+	 (c-mode . lsp)
+	 (lsp-mode . lsp-enable-which-key-integration))
   :commands (lsp lsp-deferred)
   :config
   (setq gc-cons-threshold (* 100 1024 1024)
@@ -12,20 +13,27 @@
 	  company-minimum-prefix-length 1
 	  lsp-idle-delay 0.1)  ;; clangd is fast
 
-  (use-package lsp-ui :commands lsp-ui-mode)
-  (use-package helm-lsp :commands helm-lsp-workspace-symbol)
-  (use-package dap-mode)
+  (use-package lsp-ui
+    :ensure t
+    :commands lsp-ui-mode)
+  (use-package helm-lsp
+    :ensure t
+    :commands helm-lsp-workspace-symbol)
+  (use-package dap-mode :ensure t)
+  ;; (use-package dap-cpptools :ensure t)
   (use-package dap-cpptools)
-
-  (yas-global-mode)
-
-  (lsp-enable-which-key-integration)
   
-  (lsp-register-client
-   (make-lsp-client :new-connection (lsp-tramp-connection "clangd-12")
-                    :major-modes '(c-mode c++-mode)
-                    :remote? t
-                    :server-id 'clangd-remote))
+  (use-package yasnippet
+    :ensure t
+    :config
+    (yas-global-mode))
+  
+
+  ;; (lsp-register-client
+  ;;  (make-lsp-client :new-connection (lsp-tramp-connection "clangd-12")
+  ;;                   :major-modes '(c-mode c++-mode)
+  ;;                   :remote? t
+  ;;                   :server-id 'clangd-remote))
   )
 
 (provide 'my-lsp-mode)
