39689f8f0bd80458dc6812fbe3672c3d0fbe1e8d
Soren Telfer <soren.telfer@gmail.com>
2022-10-24 17:00:45 +0000

    Modified auto-package-update options

--- init.el
+++ init.el
@@ -103,7 +103,9 @@
 (use-package auto-package-update
   :ensure t
   :config
-  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-show-preview t)
+  (setq auto-package-update-prompt-before-update t)
+  (setq auto-package-update-delete-old-versions nil)
   (setq auto-package-update-hide-results t)
   (auto-package-update-maybe))
 
